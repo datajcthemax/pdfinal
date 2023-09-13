35a23ac2e57ba0deb2e41dffd01e1a1c45cdead4
datajcthemax <data.jcthemax@gmail.com>
2023-09-13 10:50:52 +0900

    company info added

--- app/[company]/page.js
+++ app/[company]/page.js
@@ -1,74 +1,101 @@
-'use client';
+"use client";
 
-
-import { useEffect, useState } from 'react';
-import StockChart from '../components/StockChart';
-import NewsArticles from '../components/NewsArticles';
+import { useEffect, useState } from "react";
+import StockChart from "../components/StockChart";
+import NewsArticles from "../components/NewsArticles";
 
 function SymbolPage(props) {
   const [articles, setArticles] = useState([]);
   const [stockData, setStockData] = useState([]);
-  const [stockInfo, setStockInfo] = useState({});  // State for storing the stock's latest Price and Change
+  const [stockInfo, setStockInfo] = useState({}); 
+  const [companyInfo, setCompanyInfo] = useState("");
 
   const [darkMode, setDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
     if (darkMode) {
-      document.body.classList.add('dark');
+      document.body.classList.add("dark");
     } else {
-      document.body.classList.remove('dark');
+      document.body.classList.remove("dark");
     }
   };
 
   useEffect(() => {
     fetch(`/api/chart?symbol=${props.params.company}`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setStockData(data);
-        // Assuming the latest data is at the last index
-        const latestData = data[0];  // Assuming the data is sorted in descending order by date
+        const latestData = data[0];
         setStockInfo({
           Price: latestData?.Close,
-          Change: latestData?.Change
+          Change: latestData?.Change,
         });
       })
-      .catch(error => console.error('Error fetching stock data:', error));
-  }, []);
+      .catch((error) => console.error("Error fetching stock data:", error));
+  }, [props.params.company]);
+
+  useEffect(() => {
+    const fetchCompanyInfo = async () => {
+      try {
+        const response = await fetch(
+          `/api/companyInfo?symbol=${props.params.company}`  // symbol을 쿼리 파라미터로 전달
+        );
+        const data = await response.json();
+        const companyData = data[0];
+        setCompanyInfo(companyData?.longBusinessSummary || "");
+      } catch (error) {
+        console.error("Error fetching company info:", error);
+      }
+    };
+
+    fetchCompanyInfo();
+  }, [props.params.company]);
 
   useEffect(() => {
     const CURRENTS_API_KEY = process.env.NEXT_PUBLIC_CURRENTS_API_KEY;
     const ENDPOINT = `/api/currents?company=${props.params.company}`;
 
     fetch(ENDPOINT)
-      .then(response => response.json())
-      .then(data => {
-        if (data.status === 'ok') {
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "ok") {
           setArticles(data.news);
         } else {
-          console.error('Error fetching articles:', data.message);
+          console.error("Error fetching articles:", data.message);
         }
       })
-      .catch(error => console.error('Error fetching articles:', error));
+      .catch((error) => console.error("Error fetching articles:", error));
   }, [props.params.company]);
 
   return (
     <div className="p-5 dark:bg-black">
       <div className="flex justify-between items-center">
-        <h1 className="text-3xl font-bold dark:text-white">{props.params.company}</h1>
-        {/* 다크 모드 토글 버튼 */}
+        <h1 className="text-3xl font-bold dark:text-white">
+          {props.params.company}
+        </h1>
         <button onClick={toggleDarkMode} className="mr-4 text-3xl">
-          {darkMode ? '🌙' : '☀️'}
+          {darkMode ? "🌙" : "☀️"}
         </button>
       </div>
       <div className="text-2xl dark:text-gray-300">
         ${stockInfo.Price?.toFixed(2)}
       </div>
-      <div className={`text-lg mb-2 ${stockInfo.Change >= 0 ? 'text-green-500 dark:text-green-300' : 'text-red-500 dark:text-red-300'}`}>
-        {stockInfo.Change?.toFixed(2)}% {stockInfo.Change >= 0 ? '👍' : '👎'}
+      <div
+        className={`text-lg mb-2 ${
+          stockInfo.Change >= 0
+            ? "text-green-500 dark:text-green-300"
+            : "text-red-500 dark:text-red-300"
+        }`}
+      >
+        {stockInfo.Change?.toFixed(2)}% {stockInfo.Change >= 0 ? "👍" : "👎"}
       </div>
       <div className="card">
         <StockChart data={stockData} symbol={props.params.company} />
+      </div>
+      <div className="mt-4 mb-4 dark:text-white">
+        <h2 className="text-xl font-bold">Company Information</h2>
+        <p>{companyInfo}</p>
       </div>
       <NewsArticles articles={articles} />
     </div>
---
+++ pages/api/companyInfo.js
@@ -0,0 +1,35 @@
+import mysql from 'mysql2/promise';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+      });
+
+      const symbol = req.query.symbol;  // 요청에서 symbol 값을 가져옵니다.
+
+      const query = `
+      SELECT 
+        symbol, 
+        name, 
+        longBusinessSummary
+      FROM company_info
+      WHERE symbol = ?
+    `;
+
+      const [rows] = await connection.execute(query, [symbol]);  // 쿼리 실행 시 symbol 값을 전달합니다.
+      res.status(200).json(rows);
+      connection.end();
+    } catch (error) {
+      console.error('Error fetching company info:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
